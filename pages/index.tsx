696cdb655938f8ff7f54939700d1e5f6a7bf139a
Ahmad Jafari <a99jafari@gmail.com>
2022-04-26 22:12:19 +0430

    Implement Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 import type { NextPage } from "next";
+import { bitpinSnapshot } from "_/images";
+import { memo } from "react";
 
 const Home: NextPage = () => {
-  return <div>بیت‌پین</div>;
+  return (
+    <section className="my-4">
+      <Image className="rounded-3" src={bitpinSnapshot} alt="اسکرین شات وبسایت بی‌پین" />
+    </section>
+  );
 };
 
-export default Home;
+export default memo(Home);
--- public/images/index.ts
+++ public/images/index.ts
@@ -1,2 +1,3 @@
 export { default as Coin } from "./coin.svg";
 export { default as FourZeroFour } from "./not-found.svg";
+export { default as bitpinSnapshot } from "./bitpin-snapshot.png";
