12ba933adfc0e04c4c50d2149e06cc94d160f13a
Ahmad Jafari <a99jafari@gmail.com>
2022-04-27 03:38:08 +0430

    Install ChartJS library and configure it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import "../styles/globals.scss";
+import {
+  CategoryScale,
+  Chart,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
 import { Container, ThemeProvider } from "react-bootstrap";
 import { Footer, Header, NavigationMenu } from "@layouts";
 import type { AppProps } from "next/app";
+
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 function Application({ Component, pageProps }: AppProps): React.ReactElement {
   return (
