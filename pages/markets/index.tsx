ece722aaafbdfaa5e638b27e635673111b5fed81
Ahmad Jafari <a99jafari@gmail.com>
2022-04-27 01:25:35 +0430

    Implement MarketsList page

--- pages/markets/index.tsx
+++ pages/markets/index.tsx
@@ -1,46 +1,62 @@
 import { Col, Row } from "react-bootstrap";
+import { LoadingIndicator, MarketCard } from "@components";
 import { memo, useCallback, useEffect, useState } from "react";
-import { MarketCard } from "@components";
-import type { MarketsResponse } from "#/api/types";
+import { Market } from "#/types";
 import type { NextPage } from "next";
 import { fetchMarkets } from "#/api";
 
-type Market = MarketsResponse["results"];
-
 const MarketsList: NextPage = () => {
-  const [markets, setMarkets] = useState<Market>([]);
+  const [markets, setMarkets] = useState<Market[]>([]);
+  const [isDataFetched, setIsDataFetched] = useState(false);
 
   const getAndSetMarkets = useCallback(async (): Promise<void> => {
+    const cookies = document.cookie.split("; ");
     const fetchedMarkets = await fetchMarkets();
+    const transformedMarkets: Market[] = fetchedMarkets.map((market) => ({
+      id: market.id,
+      title: market.title_fa,
+      logo: market.currency1.image,
+      change: market.price_info.change,
+      code: market.code,
+      isBookmarked: cookies.some((cookie) => cookie === `${market.code}=bookmarked`),
+      price: market.price,
+    }));
 
-    setMarkets(fetchedMarkets);
+    setMarkets(transformedMarkets);
+    setIsDataFetched(true);
   }, []);
+
+  const handleToggleBookmark = useCallback(
+    (selectedMarket: Market) => {
+      const updatedMarket = markets.map((market) => {
+        if (market === selectedMarket) return { ...market, isBookmarked: !market.isBookmarked };
+        else return market;
+      });
+
+      setMarkets(updatedMarket);
+    },
+    [markets]
+  );
 
   useEffect(() => {
     getAndSetMarkets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <section className="my-4">
       <h3>لیست مارکت‌ها</h3>
-      <Row>
-        {markets.map((market) => (
-          <Col key={market.id} sm={6} md={4} lg={3}>
-            <MarketCard
-              id={market.id}
-              change={market.price_info.change}
-              code={market.code}
-              isMarked={false}
-              logo={market.currency1.image}
-              price={market.price}
-              title={market.title_fa}
-              onBookmarkButtonClick={(): void => {
-                console.log(123);
-              }}
-            />
-          </Col>
-        ))}
-      </Row>
+      {isDataFetched ? (
+        <Row>
+          {markets.map((market) => (
+            <Col key={market.id} sm={6} md={4} lg={3}>
+              <MarketCard market={market} onToggleBookmark={handleToggleBookmark} />
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <LoadingIndicator />
+      )}
     </section>
   );
 };
