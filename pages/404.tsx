58ae5c25462ef677f392cc91faaa0265de626e15
Ahmad Jafari <a99jafari@gmail.com>
2022-04-26 07:15:51 +0430

    Implement 404 screen

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import { FourZeroFour } from "_/images";
+
+function NotFound(): React.ReactElement {
+  return (
+    <div className="my-4 text-center rounded-3" id="not-found">
+      <FourZeroFour className="w-50" />
+    </div>
+  );
+}
+
+export default NotFound;
--- public/images/index.ts
+++ public/images/index.ts
@@ -1,1 +1,2 @@
 export { default as Coin } from "./coin.svg";
+export { default as FourZeroFour } from "./not-found.svg";
