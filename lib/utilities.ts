459cccc2f40cd3bd215d11a2f583b655cabbbf06
Ahmad Jafari <a99jafari@gmail.com>
2022-04-26 06:48:26 +0430

    Implement a utility function to transforming digits to Farsi

---
+++ lib/utilities.ts
@@ -0,0 +1,14 @@
+export function transformDigitsToFarsi(value: string | number): string {
+  return value
+    .toString()
+    .replaceAll("0", "۰")
+    .replaceAll("1", "۱")
+    .replaceAll("2", "۲")
+    .replaceAll("3", "۳")
+    .replaceAll("4", "۴")
+    .replaceAll("5", "۵")
+    .replaceAll("6", "۶")
+    .replaceAll("7", "۷")
+    .replaceAll("8", "۸")
+    .replaceAll("9", "۹");
+}
